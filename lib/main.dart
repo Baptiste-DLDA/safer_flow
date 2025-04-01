a06b3223d354aee18ba727e9543be5df2e939c74
Baptiste-DLDA <baptdelaverny@gmail.com>
2025-04-01 15:23:06 +0200

    La carte fonctionne

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
+
 
 class EauPotableApi {
   final String rootPath = 'https://hubeau.eaufrance.fr/api/v1/qualite_eau_potable/resultats_dis';
@@ -29,6 +35,7 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+
 class _MyAppState extends State<MyApp> {
   final EauPotableApi api = EauPotableApi();
   final TextEditingController _deptController = TextEditingController();
@@ -40,6 +47,9 @@ class _MyAppState extends State<MyApp> {
   String? _selectedParametre;
   String? _selectedYear;
   String _error = '';
+
+  LatLng? _selectedPosition;
+  final MapController _mapController = MapController();
 
   List<dynamic> _allResults = [];
 
@@ -93,7 +103,8 @@ class _MyAppState extends State<MyApp> {
           .toList();
       setState(() => _availableYears = years);
       if (years.isEmpty) {
-        setState(() => _error =
+        setState(() =>
+        _error =
         'Aucune date disponible pour ce paramètre dans ce département.');
       } else {
         setState(() => _error = '');
@@ -134,81 +145,171 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: const Text("Qualité de l'eau - Recherche")),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextField(
-                controller: _deptController,
-                decoration: const InputDecoration(
-                  labelText: 'Entrez le nom du département',
-                  border: OutlineInputBorder(),
+        body: Row(
+          children: [
+            // 🗺️ Carte interactive
+            Expanded(
+              flex: 1,
+              child: FlutterMap(
+                mapController: _mapController,
+                options: MapOptions(
+                    initialCenter: LatLng(46.603354, 1.888334), // Centre France
+                    initialZoom: 5.5,
+                    onTap: (tapPosition, point) async {
+
+                      setState(() {
+                        _selectedPosition = point; // mets à jour le marker
+                      });
+                      //_mapController.move(point, 7.5); // a enlever si on veut pas que ca s'actualise
+
+                      final lat = point.latitude;
+                      final lon = point.longitude;
+
+                      final url = Uri.parse(
+                          'https://nominatim.openstreetmap.org/reverse?lat=$lat&lon=$lon&format=json');
+
+                      try {
+                        final response = await http.get(url, headers: {
+                          'User-Agent': 'FlutterApp (bdelaverny@gmail.com)' // obligatoire pour Nominatim
+                        });
+
+                        if (response.statusCode == 200) {
+                          final data = json.decode(response.body);
+                          final address = data['address'];
+                          final departement = address['county'] ??
+                              address['state_district'] ??
+                              address['state'] ??
+                              'Département inconnu';
+                          print("Adresse complète : ${jsonEncode(address)}");
+
+                          setState(() {
+                            _deptController.text = departement;
+                          });
+
+                          print("Département détecté : $departement");
+                          fetchInitialResults(); // lance la recherche
+                        } else {
+                          print("Erreur API : ${response.statusCode}");
+                        }
+                      } catch (e) {
+                        print("Erreur reverse geocoding : $e");
+                      }
+                    }
+
                 ),
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                    "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    tileProvider: CancellableNetworkTileProvider(),
+                  ),
+
+                  if (_selectedPosition != null)
+                    MarkerLayer(
+                      markers: [
+                        Marker(
+                          point: _selectedPosition!,
+                          width: 40,
+                          height: 40,
+                          child: const Icon(
+                            Icons.location_on,
+                            color: Colors.red,
+                            size: 40,
+                          ),
+                        ),
+
+                      ],
+                    ),
+                ],
               ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: fetchInitialResults,
-                child: const Text('Valider le département'),
-              ),
-              const SizedBox(height: 10),
-              if (_availableParametres.isNotEmpty)
-                DropdownButton<String>(
-                  value: _selectedParametre,
-                  hint: const Text('Choisir un paramètre danalyse'),
-                  isExpanded: true,
-                  items: _availableParametres.map((param) {
-                    return DropdownMenuItem(
-                      value: param,
-                      child: Text(param),
-                    );
-                  }).toList(),
-                  onChanged: onParametreSelected,
-                ),
-              const SizedBox(height: 10),
-              if (_selectedParametre != null)
-                DropdownButton<String>(
-                  value: _selectedYear,
-                  hint: const Text('Choisir une année'),
-                  isExpanded: true,
-                  items: _availableYears.map((year) {
-                    return DropdownMenuItem(
-                      value: year,
-                      child: Text(year),
-                    );
-                  }).toList(),
-                  onChanged: onYearSelected,
-                  dropdownColor: _availableYears.isEmpty
-                      ? Colors.red.shade100
-                      : null,
-                ),
-              const SizedBox(height: 10),
-              if (_error.isNotEmpty)
-                Text(_error, style: const TextStyle(color: Colors.red)),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _filteredResults.length,
-                  itemBuilder: (context, index) {
-                    final item = _filteredResults[index];
-                    return ListTile(
-                      title: Text(item['libelle_unite'] ?? 'Unité inconnue'),
-                      subtitle: Text(
-                        'Commune : ${item['nom_commune'] ?? 'Inconnue'}\n'
-                            'Date prélèvement : ${item['date_prelevement'] ??
-                            'Non renseignée'}\n'
-                            'Résultat : ${item['resultat_numerique'] ?? 'N/A'}',
+            ),
+
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextField(
+                      controller: _deptController,
+                      decoration: const InputDecoration(
+                        labelText: 'Entrez le nom du département',
+                        border: OutlineInputBorder(),
                       ),
-                    );
-                  },
+                    ),
+                    const SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: fetchInitialResults,
+                      child: const Text('Valider le département'),
+                    ),
+                    const SizedBox(height: 10),
+                    if (_availableParametres.isNotEmpty)
+                      DropdownButton<String>(
+                        value: _selectedParametre,
+                        hint: const Text('Choisir un paramètre danalyse'),
+                        isExpanded: true,
+                        items: _availableParametres.map((param) {
+                          return DropdownMenuItem(
+                            value: param,
+                            child: Text(param),
+                          );
+                        }).toList(),
+                        onChanged: onParametreSelected,
+                      ),
+                    const SizedBox(height: 10),
+                    if (_selectedParametre != null)
+                      DropdownButton<String>(
+                        value: _selectedYear,
+                        hint: const Text('Choisir une année'),
+                        isExpanded: true,
+                        items: _availableYears.map((year) {
+                          return DropdownMenuItem(
+                            value: year,
+                            child: Text(year),
+                          );
+                        }).toList(),
+                        onChanged: onYearSelected,
+                        dropdownColor: _availableYears.isEmpty
+                            ? Colors.red.shade100
+                            : null,
+                      ),
+                    const SizedBox(height: 10),
+                    if (_error.isNotEmpty)
+                      Text(_error, style: const TextStyle(color: Colors.red)),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _filteredResults.length,
+                        itemBuilder: (context, index) {
+                          final item = _filteredResults[index];
+                          return ListTile(
+                            title: Text(
+                                item['libelle_unite'] ?? 'Unité inconnue'),
+                            subtitle: Text(
+                              'Commune : ${item['nom_commune'] ?? 'Inconnue'}\n'
+                                  'Date prélèvement : ${item['date_prelevement'] ??
+                                  'Non renseignée'}\n'
+                                  'Résultat : ${item['resultat_numerique'] ??
+                                  'N/A'}',
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
